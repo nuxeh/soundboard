a6c284da0ed3f30f5d869d22dffde9433d3054fe
Ed Cragg <ed.cragg@eipi.xyz>
2021-07-02 15:20:58 +0100

    Start implementation

--- src/main.ino
+++ src/main.ino
@@ -1,7 +1,17 @@
 #include <Arduino.h>
 
-void setup() {
+const int buttonPins[] = {
+  A1, A2, A3, A4,
+  10, 11, 12, A0,
+  9,  8,  7,  6,
+  2,  3,  4,  5
+};
 
+void setup() {
+  // Set all pins as inputs, enable internal pull up resistors to positive supply (Vdd)
+  for (int i=0; i<16; i++) {
+    pinMode(buttonPins[i], INPUT_PULLUP);
+  }
 }
 
 void loop() {
